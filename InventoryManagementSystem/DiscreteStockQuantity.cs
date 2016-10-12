2e80095b8f18f9f8bb3580de72e414588aa16da2
Devarsh14 <Devarsh14@gmail.com>
2016-10-13 03:50:37 +1000

    Added new class to project names discrete class using inheritance

---
+++ InventoryManagementSystem/DiscreteStockQuantity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    class DiscreteStockQuantity : StockQuantity
+    {
+        private int _Quantity;
+        public int Quantity { get; }
+
+
+
+
+    }
+}
